fdf78e0e618eb11f32979a000a84ecec60d43a4c
NekoCientifico <libraaris3.1@gmail.com>
2019-04-27 23:19:44 +0200

    Implementación a medias de Register; falta concretar algunas políticas de errores y de envío de datos, pero los datos como tal están prácticamente listos

--- app/src/main/java/com/example/ptin/RegisterActivity.java
+++ app/src/main/java/com/example/ptin/RegisterActivity.java
@@ -1,13 +1,124 @@
 package com.example.ptin;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String TAG = "RegisterActivity";
+
+
+    private EditText _usernameText = (EditText) findViewById(R.id.username);
+    private EditText _emailText = (EditText) findViewById(R.id.email);
+    private EditText _phoneText = (EditText) findViewById(R.id.phone);
+    private EditText _passwordText = (EditText) findViewById(R.id.password);
+    private EditText _confirmPasswordText = (EditText) findViewById(R.id.confirmPassword);
+
+
+    private Button button = (Button) findViewById(R.id.sign_in_button);
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.inject(this);
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+
+    }
+
+    public void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        button.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(RegisterActivity.this,
+                R.style.AppTheme_Dark); //Con AppTheme_Dark_Dialog daba error
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        String username = _usernameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String phone = _phoneText.getText().toString();
+        String password = _passwordText.getText().toString();
+        String confirmPassword = _confirmPasswordText.getText().toString();
+
+        // TODO: Implement your own signup logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    public void onSignupSuccess() {
+        button.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        button.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = _usernameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (name.isEmpty() || name.length() < 2) {
+            _usernameText.setError("at least 3 characters");
+            valid = false;
+        } else {
+            _usernameText.setError(null);
+        }
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 32) {
+            _passwordText.setError("between 4 and 32 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        return valid;
     }
 }
